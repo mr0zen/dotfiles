99975d0955755c8297635fd9c27fb7cf8ef99d44
mr0zen <mrozniecki@live.com>
2025-12-02 22:18:48 +0100

    term configs

---
+++ .config/fish/config.fish
@@ -0,0 +1,13 @@
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+if test -f /Users/majkel/anaconda3/bin/conda
+    eval /Users/majkel/anaconda3/bin/conda "shell.fish" "hook" $argv | source
+end
+# <<< conda initialize <<<
+
+
+# Added by LM Studio CLI (lms)
+set -gx PATH $PATH /Users/majkel/.lmstudio/bin
+# End of LM Studio CLI section
+
